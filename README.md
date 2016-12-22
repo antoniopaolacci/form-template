5ca24e17da961c384b93d2c7552c5206ce26237e
Antonio Paolacci <ant.paolacci@gmail.com>
2016-12-22 15:14:24 +0100

    Inital commit.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # form-template
 A simple form template
+
+
+Settings placeholder
+
+#placeholder_url_php# 		=  ...
+#placeholder_mail_to# 		=  ...
+#placeholder_url_redirect# 	=  ...
---
+++ js/send_mail.js
@@ -0,0 +1,51 @@
+function validate_send_mail() {
+
+	var nome = document.mailform.nome.value;
+	var cognome = document.mailform.cognome.value;
+	var email = document.mailform.email.value;
+	var oggetto = document.mailform.oggetto.value;
+	var messaggio = document.mailform.messaggio.value;
+	
+	if ((nome == "") || (nome == "undefined")) {
+	   errorMsg("Il campo Nome e' obbligatorio.");
+	   document.mailform.nome.focus();
+	   return false;
+    }
+	else if ((cognome == "") || (cognome == "undefined")) {
+	   errorMsg("Il campo Cognome e' obbligatorio o non corretto.");
+	   document.mailform.cognome.focus();
+	   return false;
+    }
+	else if ((email == "") || (email == "undefined") || (email.indexOf("@") == -1) || (email.indexOf(".") == -1) ) {
+	   errorMsg("Il campo Email e' obbligatorio o non corretto.");
+	   document.mailform.email.focus();
+	   return false;
+    }
+	else if ((oggetto == "") || (oggetto == "undefined")) {
+	   errorMsg("Il campo Oggetto e' obbligatorio.");
+	   document.mailform.oggetto.focus();
+	   return false;
+ 	}
+	else if ((messaggio == "") || (messaggio == "undefined")) {
+	   errorMsg("Il campo Messaggio e' obbligatorio.");
+	   document.mailform.messaggio.focus();
+	   return false;
+ 	}   
+	else { 
+	   document.mailform.action = "#placeholder_url_php#/send.php";
+	   document.mailform.submit();
+    }
+	
+}
+
+function errorMsg(text) {
+	
+	var txt = document.getElementById("validate");
+	
+	if(txt.firstChild) {
+	   txt.removeChild(txt.firstChild);	
+	}
+	
+	txt.appendChild(document.createTextNode(text));
+	
+}
